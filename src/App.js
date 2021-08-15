f5779587241d820acff7dc0738e30836f3435d52
SlavicaSkoric <skoric.slavica.hr@gmail.com>
2021-08-15 12:01:41 +0200

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Setup from './tutorial/1-useState/setup/5-useState-counter';
+// import Final from './tutorial/1-useState/final/1-error-example';
+
+function App() {
+  return (
+    <div className='container'>
+      <Setup />
+      {/* <Final /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+// useState - function, the above is a named import of it, curly braces and the exact name
+
+const UseStateBasics = () => {
+  // console.log(useState('hello world'));
+  // const value = useState(1)[0];
+  // const handler = useState(1)[1];
+  // console.log(value, handler);
+
+  // array destructuring
+  const [text, setText] = useState('random title');
+
+  const handleClick = () => {
+    if (text === 'random title') {
+      setText('hello world');
+    } else {
+      setText('random title');
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <h1>{text}</h1>
+      <button className='btn' onClick={handleClick}>
+        change title
+      </button>
+    </React.Fragment>
+  );
+};
+
+export default UseStateBasics;
